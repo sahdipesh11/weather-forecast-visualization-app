a877e1568857177d9f0999c041c94104918172dc
sahd11 <sahdipesh11@gmail.com>
2023-09-07 21:17:32 +1000

    Add the sky conditions code and images, and fix errors namely: make the temperature accurate and fix keyerror

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "9322898b88ce5000723c721f151f1749"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}" \
           f"&appid={API_KEY}"
     response = requests.get(url)
@@ -11,13 +11,9 @@ def get_data(place, forecast_days=None, kind=None):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Sky"))
+    print(get_data(place="Tokyo", forecast_days=3))
 
